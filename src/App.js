43b9684415e32f1decb4acbb721b332bba29d350
madalinmonaco <madalinmonaco@gmail.com>
2019-08-05 10:44:56 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>NC News</header>
+    </div>
+  );
+}
+
+export default App;
